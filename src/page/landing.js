8975c606667bda55c98d85f670bd737148bb783b
Clinscott <clinscott@gmail.com>
2022-01-27 13:00:16 -0500

    Setup

---
+++ src/page/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Landing extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1 className='title'>Northammer Games</h1>
+            <p className='tagline'>Howabout some games there bud.</p>
+            </div>
+        );
+    }
+}
